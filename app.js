9af5ea683f995a61bc41fd59337cb0fec9fd4387
Amar3x3 <119662755+Amar3x3@users.noreply.github.com>
2023-04-10 16:09:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,17 +10,5 @@ const nav = document.getElementById('nav-menu'),
     })
 
 
-    const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
-
-function switchTheme(e) {
-    if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-    }
-    else {
-        document.documentElement.setAttribute('data-theme', 'light');
-    }    
-}
-
-toggleSwitch.addEventListener('change', switchTheme, false);
-
+    
 
